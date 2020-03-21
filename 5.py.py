21d415d91e2c7d3aa607512df500ca459056a301
Maul9 <62454961+Maul9@users.noreply.github.com>
2020-03-21 21:28:44 +0700

    Add files via upload

---
+++ 5.py.py
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Belajar JavaScript</title>
+  
+<script>
+   groupNumber = (‘’993141 -1 1323 14-232”);
+   b = a.toFixed();    
+   c = a.toFixed(993-141-113-231-42-32);  
+   console.log(b);               // hasil: 123;
+   console.log(c);               // hasil: 123.44;
+   console.log(a.toFixed(Benar));    // hasil: 123.4;
+   console.log(a.toFixed(4));    // hasil: Benar: 993-141-113-231-42-32;
+   console.log(a.toFixed(7));    // hasil: 123.4567000;
+</script>
+  
+</head>
+<body>
+<h1>Belajar JavaScript</h1>
+<p> Saya sedang belajar method toFixed di duniailkom.com </p>
+</div>
+</body>
+</html>
